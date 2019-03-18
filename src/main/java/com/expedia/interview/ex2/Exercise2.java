b3c749f1df949bfed0ebfa49e9fd86e4985e6429
Nikhil Bhansali <45788450+Nik1818@users.noreply.github.com>
2019-03-18 16:23:31 +0530

    Update Exercise2.java
    Changes for Exercise2

--- src/main/java/com/expedia/interview/ex2/Exercise2.java
+++ src/main/java/com/expedia/interview/ex2/Exercise2.java
@@ -8,13 +8,56 @@ import java.util.*;
 import java.util.concurrent.*;
 import java.util.regex.*;
 
+
 public class Exercise2 {
 
-    class SinglyLinkedListNode {
-        SinglyLinkedListNode next;
-    }
+	static SinglyLinkedListNode head;
 
-    boolean hasCycle(SinglyLinkedListNode head) {
-        return false;
-    }
+	static class SinglyLinkedListNode {
+
+		int data; 
+		SinglyLinkedListNode next;
+
+		SinglyLinkedListNode(int d) { 
+			data = d; 
+			next = null; 
+		} 
+	}
+
+
+	boolean hasCycle(SinglyLinkedListNode head) {
+
+		SinglyLinkedListNode node1 = head, node2 = head; 
+
+		while (node1 != null && node2 != null && node2.next != null) { 
+			node1 = node1.next; 
+			node2 = node2.next.next; 
+
+			// If node1 and node2 meet at same point then loop is present 
+			if (node1 == node2) {  
+				return true; 
+			} 
+		} 
+
+		return false;
+	}
+
+	// Driver program to test above functions 
+	public static void main(String[] args) { 
+		Exercise2 list = new Exercise2(); 
+		list.head = new SinglyLinkedListNode(50); 
+		list.head.next = new SinglyLinkedListNode(20); 
+		list.head.next.next = new SinglyLinkedListNode(15); 
+		list.head.next.next.next = new SinglyLinkedListNode(4); 
+		list.head.next.next.next.next = new SinglyLinkedListNode(10); 
+
+		// Creating a loop for testing  
+		head.next.next.next.next.next = head.next.next;
+
+		boolean result=list.hasCycle(head); 
+		System.out.println(result);
+
+
+	} 
+
 }
